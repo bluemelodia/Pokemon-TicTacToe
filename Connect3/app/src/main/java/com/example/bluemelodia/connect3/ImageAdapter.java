c7e61325b4776866ccbd214f993efba3a1be7565
bluemelodia <hsumelanie@yahoo.com>
2016-01-04 17:22:59 +0800

    Created Pokemon-themed Tic Tac Toe interface.

---
+++ Connect3/app/src/main/java/com/example/bluemelodia/connect3/ImageAdapter.java
@@ -0,0 +1,77 @@
+package com.example.bluemelodia.connect3;
+
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.content.Context;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.view.View;
+
+/**
+ * Created by bluemelodia on 1/4/16.
+ */
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    // return the actual object at the specified position in the adapter
+    public Object getItem(int position) {
+        return null;
+    }
+
+    // return the row id of the item
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    /* necessary method for BaseAdapter, creates a new View for each image added to the ImageAdapter
+        when this is called, a View is passed in, which is normally a recycled object (at least after
+        this has been called once), so there's a check to see if the object is null. If it is null,
+        an ImageView is instantiated and configured with desired properties for the image presentation:
+            setLayoutParams(ViewGroup.LayoutParams) sets the height and width for the View - this ensures
+                that, no matter the size of the drawable, each image is resized and cropped to fit in these
+                dimensions, as appropriate
+            setScaleType(ImageView.ScaleType) declares that images should be cropped towards the center
+                (if necessary)
+            setPadding(int, int, int, int) defines the padding for all sides. Note that, if the images
+                have different aspect-ratios, then less padding will cause more cropping of the image
+                if it does not match the dimensions given to the ImageView
+       if the View passed to getView() is not null, then the local ImageView is initialized with the
+       recycled View object.
+     */
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(350, 350));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setAlpha(0.2f);
+            imageView.setPadding(0, 0, 0, 0);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        /* the position integer passed into the method is used to select an image from the
+            mThumbIds array, which is set as the image resource for the ImageView
+         */
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.pokemonicon, R.drawable.pokemonicon,
+            R.drawable.pokemonicon, R.drawable.pokemonicon,
+            R.drawable.pokemonicon, R.drawable.pokemonicon,
+            R.drawable.pokemonicon, R.drawable.pokemonicon,
+            R.drawable.pokemonicon
+    };
+}
---
+++ Connect3/app/src/main/java/com/example/bluemelodia/connect3/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.bluemelodia.connect3;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        // set a custom adapter (ImageAdapter) as the source for all items to be displayed in the grid
+        gridView.setAdapter(new ImageAdapter(this));
+
+        /* to do something when an item in the grid is clicked, the setOnItemClickListener() method
+            is passed a new AdapterView.OnItemClickListener. This anonymous instance defines the
+            onItemClick() callback method to show a Toast displaying the index position of the selected
+            item (in a real world scenario, the position could be used to get the full-sized image for
+            some other task) */
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
